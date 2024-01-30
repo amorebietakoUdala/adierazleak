f940cfe5e2276cb03d10ddc2e831e638fc487b19
amorebietakoUdala@github.com <informatika@amorebieta.eus>
2024-01-30 10:34:06 +0100

    Changed monolog level on prod

--- src/Controller/IndicatorController.php
+++ src/Controller/IndicatorController.php
@@ -8,7 +8,7 @@ use App\Repository\IndicatorRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
 
 #[Route(path: '/{_locale}/admin/indicator')]
 class IndicatorController extends BaseController
--- src/Controller/ObservationController.php
+++ src/Controller/ObservationController.php
@@ -11,7 +11,7 @@ use App\Repository\ObservationRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
 
 #[Route(path: '/{_locale}/observation')]
 class ObservationController extends BaseController
