0fa33d59887791a1ba06ff1aeab8f8ca8e6a5e13
amorebietakoUdala@github.com <informatika@amorebieta.eus>
2024-01-30 10:14:22 +0100

    Removed deprecations

--- src/Controller/IndicatorController.php
+++ src/Controller/IndicatorController.php
@@ -8,7 +8,7 @@ use App\Repository\IndicatorRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 
 #[Route(path: '/{_locale}/admin/indicator')]
 class IndicatorController extends BaseController
--- src/Controller/ObservationController.php
+++ src/Controller/ObservationController.php
@@ -11,7 +11,7 @@ use App\Repository\ObservationRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 
 #[Route(path: '/{_locale}/observation')]
 class ObservationController extends BaseController
