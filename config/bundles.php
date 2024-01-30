2c2fdac130ca348ebd952d5d2809dc8d3180747d
amorebietakoUdala@github.com <informatika@amorebieta.eus>
2024-01-30 10:23:14 +0100

    Removed deprecations and removed monolog

--- config/bundles.php
+++ config/bundles.php
@@ -10,7 +10,6 @@ return [
     FOS\JsRoutingBundle\FOSJsRoutingBundle::class => ['all' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
-    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
     Symfony\UX\Turbo\TurboBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
